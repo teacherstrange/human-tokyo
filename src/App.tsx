c3a48146717d2af6ea2ae52453441af2b2adf264
David Paulsson <david@davidpaulsson.se>
2023-03-09 14:15:29 +0100

    chore: add types

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,7 @@ export default function App() {
     <>
       <Header />
       <LocomotiveScrollProvider
-        options={{
-          smooth: true,
-          // ... all available Locomotive Scroll instance options
-        }}
-        watch={
-          [
-            //..all the dependencies you want to watch to update the scroll.
-            //  Basicaly, you would want to watch page/location changes
-            //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-          ]
-        }
+        options={{ smooth: true }}
         containerRef={containerRef}
       >
         <main className="root" data-scroll-container ref={containerRef}>
