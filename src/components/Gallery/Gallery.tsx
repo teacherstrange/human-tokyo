6cfbb25a85a9602992f4a5068718b6149a7c0951
David Paulsson <david@davidpaulsson.se>
2023-03-26 10:33:38 +0200

    style: update black to match video

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,66 +1,95 @@
 import { StaticImage } from 'gatsby-plugin-image';
+import { IStaticImageProps } from 'gatsby-plugin-image/dist/src/components/static-image.server';
 import React from 'react';
 import Video from '../Video';
 
 import * as styles from './Gallery.module.scss';
 
+const GalleryItem = ({ staticImage }: { staticImage: JSX.Element }) => {
+  return staticImage;
+};
+
 export default function Gallery() {
   return (
     <div className={styles.gallery}>
       <Video />
-      <StaticImage
-        className={styles.image}
-        src="./assets/images/ht-closeup-1.jpg"
-        alt=""
-        placeholder="blurred"
-        layout="fullWidth"
-        formats={['auto', 'webp', 'avif']}
-        quality={80}
+      <GalleryItem
+        staticImage={
+          <StaticImage
+            className={styles.image}
+            src="./assets/images/ht-closeup-1.jpg"
+            alt=""
+            placeholder="blurred"
+            layout="fullWidth"
+            formats={['auto', 'webp', 'avif']}
+            quality={80}
+          />
+        }
       />
-      <StaticImage
-        className={styles.image}
-        src="./assets/images/ht-closeup-2.jpg"
-        alt=""
-        placeholder="blurred"
-        layout="fullWidth"
-        formats={['auto', 'webp', 'avif']}
-        quality={80}
+      <GalleryItem
+        staticImage={
+          <StaticImage
+            className={styles.image}
+            src="./assets/images/ht-closeup-2.jpg"
+            alt=""
+            placeholder="blurred"
+            layout="fullWidth"
+            formats={['auto', 'webp', 'avif']}
+            quality={80}
+          />
+        }
       />
-      <StaticImage
-        className={styles.image}
-        src="./assets/images/ht-closeup-3.jpg"
-        alt=""
-        placeholder="blurred"
-        layout="fullWidth"
-        formats={['auto', 'webp', 'avif']}
-        quality={80}
+      <GalleryItem
+        staticImage={
+          <StaticImage
+            className={styles.image}
+            src="./assets/images/ht-closeup-3.jpg"
+            alt=""
+            placeholder="blurred"
+            layout="fullWidth"
+            formats={['auto', 'webp', 'avif']}
+            quality={80}
+          />
+        }
       />
-      <StaticImage
-        className={styles.image}
-        src="./assets/images/ht-poster-1.jpg"
-        alt=""
-        placeholder="blurred"
-        layout="fullWidth"
-        formats={['auto', 'webp', 'avif']}
-        quality={80}
+      <GalleryItem
+        staticImage={
+          <StaticImage
+            className={styles.image}
+            src="./assets/images/ht-poster-1.jpg"
+            alt=""
+            placeholder="blurred"
+            layout="fullWidth"
+            formats={['auto', 'webp', 'avif']}
+            quality={80}
+          />
+        }
       />
-      <StaticImage
-        className={styles.image}
-        src="./assets/images/ht-poster-2.jpg"
-        alt=""
-        placeholder="blurred"
-        layout="fullWidth"
-        formats={['auto', 'webp', 'avif']}
-        quality={80}
+      <GalleryItem
+        staticImage={
+          <StaticImage
+            className={styles.image}
+            src="./assets/images/ht-poster-2.jpg"
+            alt=""
+            placeholder="blurred"
+            layout="fullWidth"
+            formats={['auto', 'webp', 'avif']}
+            quality={80}
+          />
+        }
       />
-      <StaticImage
-        className={styles.image}
-        src="./assets/images/ht-poster-3.jpg"
-        alt=""
-        placeholder="blurred"
-        layout="fullWidth"
-        formats={['auto', 'webp', 'avif']}
-        quality={80}
+      <GalleryItem
+        staticImage={
+          <StaticImage
+            className={styles.image}
+            src="./assets/images/ht-poster-3.jpg"
+            alt=""
+            placeholder="blurred"
+            layout="fullWidth"
+            formats={['auto', 'webp', 'avif']}
+            quality={80}
+          />
+        }
       />
     </div>
   );
